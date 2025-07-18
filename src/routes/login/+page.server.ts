fd5233ec38e7a6b6177384c6601e2f297c46e3de
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2025-07-18 23:08:03 +0700

    make login page perform login immediately instead of showing ui.

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,11 +1,13 @@
 import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 import { variables } from "$lib/variables.server";
+import { browseURL } from "$lib/routes";
 
 export const load: PageServerLoad = async ({ url, fetch }) => {
     const target = url.searchParams.get('target')
-    if (target == null) {
-        return
+
+    if (!variables.oidcEnable) {
+        redirect(307, target ?? browseURL(url.origin))
     }
 
     const oidpUrl = new URL(variables.oidcAuth)
@@ -13,7 +15,7 @@ export const load: PageServerLoad = async ({ url, fetch }) => {
     oidpUrl.searchParams.set('scope', 'openid email profile')
     oidpUrl.searchParams.set('client_id', variables.oidcClient)
     oidpUrl.searchParams.set('redirect_uri', new URL("/login/callback", url.origin).toString())
-    oidpUrl.searchParams.set('state', target)
+    oidpUrl.searchParams.set('state', target ?? browseURL(url.origin).toString())
 
     redirect(307, oidpUrl)
 }
