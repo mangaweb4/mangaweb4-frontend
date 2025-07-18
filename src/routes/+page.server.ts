e39937071cdf2c5f921ad65532f4a36da1663aae
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2025-07-18 21:34:29 +0700

    update imports.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,8 @@
 import type { PageServerLoad } from './$types';
 import { variables } from '$lib/variables.server';
 import { redirect } from '@sveltejs/kit'
-import { browseURL } from '$lib/routes'
+import { browseURL, loginUrl } from '$lib/routes'
+import logger from '$lib/logger';
 
 export const load: PageServerLoad = async ({ url, cookies }) => {
 
