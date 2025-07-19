135be6171555b89325dfedbf896c03f65bdd0895
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2025-07-19 17:03:56 +0700

    add aurhtorization token handling.

--- src/lib/auth.server.ts
+++ src/lib/auth.server.ts
@@ -6,6 +6,42 @@ import logger from '$lib/logger'
 
 const JWKS = variables.oidcEnable ? jose.createRemoteJWKSet(new URL(variables.oidcJWKS)) : null
 
+export async function validateSessionWithHeader(url: URL, request: Request) {
+    if(!variables.oidcEnable) {
+        return;
+    }
+
+    if (!JWKS) {
+        return
+    }
+
+    const authorization = request.headers.get("Authorization")
+    if (authorization == null) {
+        return
+    }
+
+    const regex = /Bearer (.*)/
+    const matches = regex.exec(authorization)
+
+    if (matches == null || matches.length < 2){
+        throw new Error("invalid Authorization header")
+    }
+
+    const accessToken = matches[1]
+
+    try {
+        const { payload, protectedHeader } = await jose.jwtVerify(accessToken, JWKS, {
+            issuer: variables.oidcIssuer,
+            audience: variables.oidcClient,
+        })
+
+    } catch (err: any) {
+        logger.debug(err, 'validate JWT error')
+        throw new Error("invalid Authorization header")
+    }
+
+}
+
 export async function validateSession(url: URL, cookies: Cookies) {
     if (!variables.oidcEnable) {
         return;
