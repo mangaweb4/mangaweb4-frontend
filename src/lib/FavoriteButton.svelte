6a9dbeba47306e53f2d4fc4fbeddd79cd6f224a3
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2025-07-29 04:53:50 +0700

    replace fixed margin on icon.

--- src/lib/FavoriteButton.svelte
+++ src/lib/FavoriteButton.svelte
@@ -2,13 +2,13 @@
 	import { Icon } from '@sveltestrap/sveltestrap';
 	import './custom.scss';
 
-	let { isFavorite = false, children, onclick } = $props();
+	let { isFavorite = false, children = null, onclick } = $props();
 </script>
 
 <button class="btn" class:btn-pink={isFavorite} class:btn-outline-pink={!isFavorite} {onclick}>
 	{#if isFavorite}
-		<span><Icon name="star-fill" class="me-3" /> {@render children?.()}</span>
+		<span><Icon name="star-fill"/> {@render children?.()}</span>
 	{:else}
-		<span><Icon name="star" class="me-3" /> {@render children?.()}</span>
+		<span><Icon name="star"/> {@render children?.()}</span>
 	{/if}
 </button>
