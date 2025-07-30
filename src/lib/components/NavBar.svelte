e239287691267d01ec6026c91afc19a2254581a0
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2025-07-31 00:04:22 +0700

    update branding

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
 	import { Icon } from 'svelte-icon';
 	import menu from '@mdi/svg/svg/menu.svg?raw';
-
+	import logo from '$lib/logo.svg?raw';
 	let { title, showMenu = $bindable() } = $props();
 </script>
 
 <div class="navbar bg-base-100 shadow-sm sticky top-0 z-1">
 	<div class="w-full max-w-[1024px] mx-auto flex">
-		<div class="flex-1">
-			<p class="text-xl">{title}</p>
+		<div class="flex-none">
+			<Icon data={logo} width="128px" height="48px" />
+		</div>
+		<div class="flex-1 m-2">
+			<div class="text-xl">{title}</div>
 		</div>
 		<div class="flex-none">
 			<button class="btn btn-square btn-ghost" onclick={() => (showMenu = true)}>
