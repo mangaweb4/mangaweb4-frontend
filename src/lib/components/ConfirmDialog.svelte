0cafa1d5c35280fe50b310c3ea725253fe9d2fc7
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2025-07-31 21:50:38 +0700

    update confirm dialog.

---
+++ src/lib/components/ConfirmDialog.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	let dialog: HTMLDialogElement;
+
+	let headerStr = $state('');
+	let bodyStr = $state('');
+	let onOkFunc = $state((): void => {});
+
+	export function show(header: string, body: string, onOk: () => void) {
+		headerStr = header;
+		bodyStr = body;
+		onOkFunc = onOk;
+		dialog.showModal();
+	}
+</script>
+
+<dialog class="modal" bind:this={dialog}>
+	<div class="modal-box">
+		<h3 class="text-lg font-bold">Now Loading</h3>
+		<p class="py-4">
+			<span class="loading loading-spinner"></span>&nbsp;Please wait.
+		</p>
+		<div class="modal-action">
+			<form method="dialog">
+				<button
+					class="btn btn-primary"
+					onclick={() => {
+						onOkFunc();
+						dialog.close();
+					}}
+				>
+					OK
+				</button>
+				<button class="btn btn-secondary" onclick={() => dialog.close()}> Cancel </button>
+			</form>
+		</div>
+	</div>
+</dialog>
