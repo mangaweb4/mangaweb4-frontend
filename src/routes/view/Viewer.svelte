a130f8b54b729ba378f252dee4a2bc1aec7e2185
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2025-07-31 07:26:34 +0700

    reposition view control.

--- src/routes/view/Viewer.svelte
+++ src/routes/view/Viewer.svelte
@@ -43,14 +43,14 @@
 </script>
 
 <button
-	class="btn text-gray-500/50 hover:text-gray-500 bg-transparent border-transparent shadow-none fixed inset-y-1/4 h-1/2 z-10 w-20 start-2"
+	class="btn text-gray-500/50 hover:text-gray-500 bg-transparent border-transparent shadow-none fixed inset-y-1/2 -translate-y-1/2 h-1/2 z-10 w-20 start-2"
 	onclick={() => emblaApi?.scrollPrev()}
 >
 	<Icon data={chevronLeft}></Icon>
 </button>
 
 <button
-	class="btn text-gray-500/50 hover:text-gray-500 bg-transparent border-transparent shadow-none fixed inset-y-1/4 h-1/2 z-10 w-20 end-2"
+	class="btn text-gray-500/50 hover:text-gray-500 bg-transparent border-transparent shadow-none fixed inset-y-1/2 -translate-y-1/2 h-1/2 z-10 w-20 end-2"
 	onclick={() => emblaApi?.scrollNext()}
 >
 	<Icon data={chevronRight}></Icon>
@@ -71,7 +71,7 @@
 </div>
 
 <button
-	class="btn text-gray-500/50 hover:text-gray-500 bg-transparent border-transparent shadow-none fixed inset-x-1/4 w-1/2 z-10 h-20 bottom-2"
+	class="btn text-gray-500/50 hover:text-gray-500 bg-transparent border-transparent shadow-none fixed inset-x-1/2 -translate-x-1/2 w-1/2 z-10 h-20 bottom-2"
 	onclick={() => pageScroll.showModal()}><Icon data={chevronUp}></Icon></button
 >
 
