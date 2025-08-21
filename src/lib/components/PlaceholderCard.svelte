3198a7ad2e793710172c387881225806ff144c95
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2025-08-22 00:35:21 +0700

    update placeholder.

--- src/lib/components/PlaceholderCard.svelte
+++ src/lib/components/PlaceholderCard.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import placeholderThumbnail from '@mdi/svg/svg/timer-sand.svg?raw';
-	import { Icon } from 'svelte-icon';
 	interface Props {
 		accessTime?: boolean;
 	}
@@ -10,31 +8,23 @@
 
 <div class="card bg-base-100 h-full shadow-xl">
 	<div class="mt-0 mb-0">
-		<div style="display:block; aspect-ratio: 1/1.414">
-			<Icon
-				class="card-img-top h-full"
-				data={placeholderThumbnail}
-				color="gray"
-				width="359"
-				height="510"
-				viewBox="0 -8 24 48"
-			/>
+		<div class="aspect-[1/1.414] relative">
+			<div class="absolute place-self-center inset-1/2 skeleton h-4 w-full h-full"></div>
 		</div>
 	</div>
 	<div class="card-body">
-		<div class="h-[4em] overflow-hidden">
-			<div class="skeleton h-4 w-70 mb-1"></div>
-			<div class="skeleton h-4 w-28"></div>
+		<div class="h-32 overflow-hidden">
+			<div class="skeleton h-4 w-70 m-2 mx-auto"></div>
+			<div class="skeleton h-4 w-50 m-2 mx-auto"></div>
+			<div class="skeleton h-4 w-70 m-2 mx-auto"></div>
+			<div class="skeleton h-4 w-28 m-2 mx-auto"></div>
 		</div>
 
 		{#if accessTime}
+			<div class="divider"><div class="skeleton h-4 w-50"></div> </div>
 			<div class="h-[2em] overflow-hidden">
 				<div class="skeleton h-4 w-50"></div>
 			</div>
 		{/if}
-
-		<div class="h-[2em] overflow-hidden">
-			<div class="skeleton h-4 w-50 mb-1"></div>
-		</div>
 	</div>
 </div>
