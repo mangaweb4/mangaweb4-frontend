ada156972b13b722ea627d1bc45f249daeef5280
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2025-09-03 00:30:46 +0700

    set background color to bg-base-300

--- src/lib/components/Container.svelte
+++ src/lib/components/Container.svelte
@@ -2,7 +2,7 @@
 	let { showMenu = $bindable(false), children } = $props();
 </script>
 
-<div class="drawer-end">
+<div class="drawer-end bg-base-300">
 	<input type="checkbox" class="drawer-toggle" bind:checked={showMenu} />
 	{@render children?.()}
 </div>
